339eae446b9ed7d9d2d390904258bde131048132
Alina-Makarenko <alinamakarenko1807@gmail.com>
2025-08-29 22:26:44 +0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,8 @@
+const age = "16";
+console.log(age);
+const name = "Alina";
+console.log(name);
+const isStudent = "true";
+console.log(isStudent);
+const myString = "A person's freedom in the modern world is like the freedom of a person solving a crossword puzzle: theoretically, they can enter any word, although in reality they only have to enter one for the crossword puzzle to be solved.";
+console.log(myString);
