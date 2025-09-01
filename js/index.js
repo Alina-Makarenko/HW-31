010137990e41f15cb655710b78b5ca1893538417
Alina-Makarenko <alinamakarenko1807@gmail.com>
2025-09-01 08:49:21 +0300

    commit 2

--- js/index.js
+++ js/index.js
@@ -6,3 +6,14 @@ const isStudent = "true";
 console.log(isStudent);
 const myString = "A person's freedom in the modern world is like the freedom of a person solving a crossword puzzle: theoretically, they can enter any word, although in reality they only have to enter one for the crossword puzzle to be solved.";
 console.log(myString);
+const myNumber = 345;
+const number = 10;
+console.log(myNumber + number);
+const myNull = "null";
+console.log(myNull);
+const valuA = 'string';
+console.log(Number(valuA));
+const word = "hello World";
+console.log(word)
+
+
