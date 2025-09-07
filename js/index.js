4685f5377c300f0338a5e8cde798d559298cbe5b
Alina-Makarenko <alinamakarenko1807@gmail.com>
2025-09-07 14:53:58 +0300

    commit 7

--- js/index.js
+++ js/index.js
@@ -1,22 +1,63 @@
-const age = "16";
-console.log(age);
-const name = "Alina";
-console.log(name);
-const isStudent = true;
-console.log(isStudent);
-const myString = "A person's freedom in the modern world is like the freedom of a person solving a crossword puzzle: theoretically, they can enter any word, although in reality they only have to enter one for the crossword puzzle to be solved.";
-console.log(myString);
-const number = 345;
-const myNumber = number + 10;
-console.log(myNumber);
-const myNull = null;
-console.log(myNull);
-const userName = prompt("Введіть своє ім’я");
-alert(userName);
-console.log(typeof (userName));
-const userAnswer = confirm("Ви дійсно хочете покинути сторінку?");
-console.log(userAnswer);
-const message = "Увага! Дія може бути небезпечною.";
-const warning = confirm("Ви впевнені, що хочете продовжити?");
-alert(message);
+// HW 31
+// const age = "16";
+// console.log(age);
+// const name = "Alina";
+// console.log(name);
+// const isStudent = true;
+// console.log(isStudent);
+// const myString = "A person's freedom in the modern world is like the freedom of a person solving a crossword puzzle: theoretically, they can enter any word, although in reality they only have to enter one for the crossword puzzle to be solved.";
+// console.log(myString);
+// const number = 345;
+// const myNumber = number + 10;
+// console.log(myNumber);
+// const myNull = null;
+// console.log(myNull);
+// const userName = prompt("Введіть своє ім’я");
+// alert(userName);
+// console.log(typeof (userName));
+// const userAnswer = confirm("Ви дійсно хочете покинути сторінку?");
+// console.log(userAnswer);
+// const message = "Увага! Дія може бути небезпечною.";
+// const warning = confirm("Ви впевнені, що хочете продовжити?");
+// alert(message);
+
+// HW 32
+const celsius = 20;
+const fahrenheit = (celsius * 9 / 5) + 32;
+console.log(fahrenheit);
+const daysInMonth = 30;
+const hoursInMonth = daysInMonth * 24;
+console.log(hoursInMonth);
+const minutesInMonth = hoursInMonth * 60;
+console.log(minutesInMonth);
+const health = 100 - 40;
+console.log(health);
+const energy = 500 - 100;
+console.log(energy);
+const totalPrice = 500;
+const discount = 10;
+const discountedPrice = (totalPrice * discount)/100;
+console.log(discountedPrice);
+const floatNumber = 12.78;
+const numbers = Math.floor(floatNumber)
+console.log(numbers);
+const floatString = "45.67";
+const string = parseFloat(floatString);
+console.log(string);
+const intString = "123";
+const parsedInt = parseInt(intString);
+console.log(parsedInt);
+const number = 9;
+const result = Math.sqrt(number);
+console.log(result);
+const integer = 42;
+const convertedString = integer.toString();
+console.log(convertedString);
+const stringNumber = "256";
+const convertedInt = parseInt(stringNumber);
+console.log(convertedInt);
+
+
+
+
 
