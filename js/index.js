99e09733cb323c00b02f3f45a237b4ae9c5a0a93
Alina-Makarenko <alinamakarenko1807@gmail.com>
2025-09-29 12:41:18 +0300

    ggggg

--- js/index.js
+++ js/index.js
@@ -60,25 +60,59 @@
 // console.log(convertedInt);
 
 // HW-33
-const result = 5 + 5 + "5";
-console.log(result);
-console.log(typeof(result));
-const email = "alina1807@gmail.com";
-console.log(email.includes("@"));
-console.log(email.length);
-const my = "My";
-const names = "name";
-const is = "is";
-const v = "Viktor";
-const fullName =  my + "," + " " + names + "," + " " + is + " ";
-console.log(fullName + v + ".");
-const userName = "Олександро";
-const payment = 300;
-console.log(alert(`Дякуємо, ${userName}! До сплати ${payment} гривень`));
+// const result = 5 + 5 + "5";
+// console.log(result);
+// console.log(typeof(result));
+// const email = "alina1807@gmail.com";
+// console.log(email.includes("@"));
+// console.log(email.length);
+// const my = "My";
+// const names = "name";
+// const is = "is";
+// const v = "Viktor";
+// const fullName =  my + "," + " " + names + "," + " " + is + " ";
+// console.log(fullName + v + ".");
+// const userName = "Олександро";
+// const payment = 300;
+// console.log(alert(`Дякуємо, ${userName}! До сплати ${payment} гривень`));
 
+// HW-34
+const a = "Введіть текст";
+const b = "Введіть коментар";
+if ( a !== null && b !== null) {
+    console.log("Обидва поля заповнені");
+}else{
+    console.log("Не всі поля заповнені");
+};
 
+const cost1 = 4;
+const cost2 = 5;
+const cost = cost1 + cost2;
+if (cost > 10){
+    console.log("Сума більша за 10");
+}else if(cost <= 10){
+    console.log("Сума менша або дорівнює 10");
+}
 
+const text = "JavaScript hbjhbjhvjhvvhvjhvjhjhhjhgkjkjhkjhkjhk";
+if (text.includes("JavaScript")){
+    console.log("Текст містить слово JavaScript");
+}else{
+    console.log("Текст не містить слово JavaScript");
+    
+}
 
+const number = Number(prompt("Введіть число"));
+if ( 10 <= number && number <= 20){
+    console.log("Число входить в діапазон від 10 до 20");
+}else{
+    console.log("Число не входить в діапазон від 10 до 20");
+}
 
-
-
+const name1 = "Filip";
+const email = "ckmvkn@.edce";
+const password = "32534543543";
+if ( name1.length >= 3 && email.includes("@.") && password.length >= 6){
+    console.log("Перенаправлення на іншу сторінку");
+}else{ console.log("Помилка: неправильне заповнення");
+}
